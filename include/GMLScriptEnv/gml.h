89f6d458ceb972d7801313bcb9dca19b9884e3b2
Rai <56082069+raicool@users.noreply.github.com>
2026-01-15 17:54:02 -0500

    fixed function parameter annotations

--- include/GMLScriptEnv/gml.h
+++ include/GMLScriptEnv/gml.h
@@ -11,11 +11,11 @@ struct func_info
 };
 
 typedef GMLVar& (*GMLScriptPtr)(
-	IN GMLInstance* self, 
-	IN GMLInstance* other, 
-	OUT GMLVar& out, 
-	IN int argCount, 
-	IN GMLVar* args
+	_In_ GMLInstance* self, 
+	_In_ GMLInstance* other,
+	_Out_ GMLVar& out, 
+	_In_ int argCount,
+	_In_ GMLVar* args
 );
 
 static RVariableRoutine* builtin_array = nullptr;
@@ -24,22 +24,22 @@ static int32_t* builtin_count = nullptr;
 std::string __gml_setup();
 
 func_info __impl_get_func_info(
-	IN std::string name
+	_In_ std::string name
 );
 
 func_info __impl_get_func_info(
-	IN const char* name
+	_In_ const char* name
 );
 
 // Call a built-in GM function using its numerical ID
 // Arguments are passed by array
 GMLVar* __impl_gml_call_func(
-	IN int func_id,
-	IN int arg_count, 
-	IN GMLVar** args
+	_In_ int func_id,
+	_In_ int arg_count,
+	_In_ GMLVar** args
 );
 
 size_t __impl_get_builtin_variable_index(
-	IN const char* name
+	_In_ const char* name
 );
 
