97d7c401df3cff1f96baf567620b77e4d77e3712
Rai <56082069+raicool@users.noreply.github.com>
2026-01-15 17:37:46 -0500

    add __impl prefix to some functions

--- include/GMLScriptEnv/gml.h
+++ include/GMLScriptEnv/gml.h
@@ -1,10 +1,8 @@
 #pragma once
-#include <string>
 
 #include "gml_types.h"
-#include <string>
 
-typedef GMLVar& (*GMLScriptPtr)(GMLInstance* self, GMLInstance* other, GMLVar& out, int argCount, GMLVar** args);
+#include <string>
 
 struct func_info
 {
@@ -12,17 +10,36 @@ struct func_info
 	void* ptr;
 };
 
-std::string __gml_setup();
-
-func_info get_func_info(std::string name);
-func_info get_func_info(const char* name);
+typedef GMLVar& (*GMLScriptPtr)(
+	IN GMLInstance* self, 
+	IN GMLInstance* other, 
+	OUT GMLVar& out, 
+	IN int argCount, 
+	IN GMLVar* args
+);
 
 static RVariableRoutine* builtin_array = nullptr;
 static int32_t* builtin_count = nullptr;
 
+std::string __gml_setup();
+
+func_info __impl_get_func_info(
+	IN std::string name
+);
+
+func_info __impl_get_func_info(
+	IN const char* name
+);
+
 // Call a built-in GM function using its numerical ID
 // Arguments are passed by array
-GMLVar* gml_call_func(int functionID, int argCount, GMLVar** args);
-size_t get_builtin_variable_index(const char* name);
-//GMLScriptPtr getScriptPtr(int scriptID);
+GMLVar* __impl_gml_call_func(
+	IN int func_id,
+	IN int arg_count, 
+	IN GMLVar** args
+);
+
+size_t __impl_get_builtin_variable_index(
+	IN const char* name
+);
 
